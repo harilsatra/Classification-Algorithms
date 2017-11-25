46281a866343fa1a673e1ff074a47b998dffae23
Haril Satra <harilsatra94@gmail.com>
2017-11-25 18:57:10 -0500

    KNN & Naive Bayesian Finished

--- nbayes.py
+++ nbayes.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov  7 02:00:28 2017
+Created on Wed Nov 22 19:42:02 2017
 
 @author: harilsatra
 """
-
 import numpy as np
-from sklearn import preprocessing
-from sklearn.metrics.pairwise import euclidean_distances
-import math
-
+from scipy.stats import norm
 #http://pythoncentral.io/how-to-check-if-a-string-is-a-number-in-python-including-unicode/
 # Function to check if a string is a number or not
 def is_number(s):
@@ -28,34 +24,15 @@ def is_number(s):
     
     return False
 
-# Function to calculate the distance between two samples
-def euclidean_dist(mat,cat_var):
-    no_samples = len(mat)
-    dist_mat = np.zeros((no_samples,no_samples))
-    for i in range(0,no_samples):
-        for j in range(i,no_samples):
-            sum = 0
-            if i==j:
-                dist_mat[i][j] = 0
-            else:
-                for k in range(len(mat[0])):
-                    if k not in cat_var:
-                        sum += (mat[i][k] - mat[j][k])**2
-                    else:
-                        if mat[i][k] == mat[j][k]:
-                            sum += 1
-                dist_mat[i][j] = dist_mat[j][i] = math.sqrt(sum)
-    return dist_mat
-
 # Prompt the user to enter the filename
-filename = input("Enter the filename with extension: ")
+#filename = input("Enter the filename with extension: ")
 
 # Read the file specified by the user
-with open(filename) as textFile:
+with open('project3_dataset2.txt') as textFile:
     lines = [line.split('\t') for line in textFile]
 
 no_samples = len(lines)
-no_attrs = len(lines[0])-1
+no_attrs = len(lines[0])
               
 data = np.zeros((no_samples,no_attrs),dtype=float)
 class_labels = [int(row[-1].rstrip("\n")) for row in lines]
@@ -77,16 +54,7 @@ for j in range(no_attrs):
             nominal_attr[lines[i][j]] = nominal_count
             nominal_count += 1
 
-#http://scikit-learn.org/stable/modules/preprocessing.html
-# Normalize the data so that certain attributes do not dictate the distance
-min_max_scaler = preprocessing.MinMaxScaler()
-normalized_data = min_max_scaler.fit_transform(data)
-
-# Obtain the distance matrix
-dist_mat = euclidean_dist(normalized_data,cat_attrs)
-
 k = 10 # K-fold value
-nn= 4 # Number of nearest neighbours
 
 # Initialize the evaluation metrics
 accuracy = 0
@@ -94,7 +62,7 @@ precision = 0
 recall = 0
 f1 = 0
 
-test_len = int(no_samples/k)
+test_len = int(no_samples/10)
 train_len = no_samples - test_len
 start = 0
 end = test_len
@@ -103,30 +71,59 @@ end = test_len
 for fold in range(k):
     
     # Extract the test data and training data
-    test_data = normalized_data[start:end][:]
+    test_data = data[start:end][:]
     test_true_labels = class_labels[start:end]
-    train_data = np.delete(normalized_data,np.s_[start:end],0)
+    train_data = np.delete(data,np.s_[start:end],0)
     train_labels = np.delete(class_labels,np.s_[start:end])
     
-    distances = dist_mat[start:end][:]
-    distances = np.delete(distances,np.s_[start:end],axis=1)
+    temp_0 = train_data[train_data[:,-1]==0,:]
+    temp_1 = train_data[train_data[:,-1]==1,:]
+    
+    prior_0 = len(temp_0)/len(train_data)
+    prior_1 = len(temp_1)/len(train_data)
+    
+    mean_0 = temp_0.mean(0)
+    mean_1 = temp_1.mean(0)
+    
+    std_0 = temp_0.std(0,ddof=1)
+    std_1 = temp_1.std(0,ddof=1)
+    
+    cat_count_0 = {}
+    cat_count_1 = {}
+    
+    for attr_index in cat_attrs:
+        temp_list_0 = np.unique(temp_0[:,attr_index],return_counts=True)
+        temp_dict_0 = dict(zip(temp_list_0[0],temp_list_0[1]))
+        cat_count_0[attr_index] = temp_dict_0
+        
+        temp_list_1 = np.unique(temp_1[:,attr_index],return_counts=True)
+        temp_dict_1 = dict(zip(temp_list_1[0],temp_list_1[1]))
+        cat_count_1[attr_index] = temp_dict_1
+    
     test_new_labels = []
-    for dist in distances:
-        classes = {}
-        idx = np.argpartition(dist,nn)
-        #print([train_labels[temp] for temp in idx[0:nn]])
-        for x in range(0,nn):
-            classes[train_labels[idx[x]]] = classes.get(train_labels[idx[x]],0)+1
-    
-        #print(str(classes))
-        election = 0
-        label = -1
-        for key in classes.keys():
-            if classes[key] > election:
-                election = classes[key]
-                label = key
-        test_new_labels.append(label)
-    
+    for i in range(0,len(test_data)):
+        posterior_0 = norm(mean_0,std_0).pdf(test_data[i])
+        posterior_1 = norm(mean_1,std_1).pdf(test_data[i])
+        post_prob_0 = 1
+        post_prob_1 = 1
+        for j in range(0,no_attrs-1):
+            if j in cat_attrs:
+                prob_0 = cat_count_0[j][test_data[i][j]]/len(temp_0)
+                prob_1 = cat_count_1[j][test_data[i][j]]/len(temp_1)
+                post_prob_0 *= prob_0
+                post_prob_1 *= prob_1
+                
+            else:
+                post_prob_0 *= posterior_0[j]
+                post_prob_1 *= posterior_1[j]
+         
+        final_prob_0 = prior_0 * post_prob_0
+        final_prob_1 = prior_1 * post_prob_1
+        if final_prob_0 > final_prob_1:
+            test_new_labels.append(0)
+        else:
+            test_new_labels.append(1)
+                
     # Populate the Confusion matrix in order to calculate the evaluation metrics.
     confusion_matrix = np.zeros((2,2))
     for i in range(len(test_true_labels)):
@@ -147,14 +144,12 @@ for fold in range(k):
     recall += confusion_matrix[0][0]/(confusion_matrix[0][0]+confusion_matrix[0][1])
     f1 += (2*confusion_matrix[0][0])/((2*confusion_matrix[0][0])+confusion_matrix[0][1]+confusion_matrix[1][0])
     
-    start += test_len
-    end += test_len
-
-
-    #print("TRUE LABELS: ")
-    #print(test_true_labels)
-    #print("NEW LABELS: ")
-    #print(test_new_labels)
+    if fold==8:
+        start += test_len
+        end += test_len+no_samples-(10*test_len)
+    else:
+        start += test_len
+        end += test_len
 
 print("Accuracy: " ,accuracy/k)
 print("Precision: " ,precision/k)
